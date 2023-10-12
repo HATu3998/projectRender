0c6f282eee7f5f078a2e32cb4c92c9457f8b5d57
AnhTu <3artemis98@gmail.com>
2023-10-13 04:50:15 +0700

    datasour

--- src/main/java/com/example/RenderProject/security/DataSourceConfig.java
+++ src/main/java/com/example/RenderProject/security/DataSourceConfig.java
@@ -9,13 +9,23 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/apidemo");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("");
+//        return dataSource;
+//    }
+    
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/apidemo");
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
+        dataSource.setDriverClassName("org.postgresql.Driver"); // Sử dụng driver của PostgreSQL
+        dataSource.setUrl("jdbc:postgresql://dpg-ckigog212bvs738pfmvg-a.oregon-postgres.render.com:5432/renderproduct");
+        dataSource.setUsername("renderproduct_user");
+        dataSource.setPassword("1AB43lwOucjXHoAHeBgONXYZoZH5vtKW");
         return dataSource;
     }
 }
